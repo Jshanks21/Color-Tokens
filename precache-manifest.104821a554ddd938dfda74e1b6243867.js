a708dcec2ad68db58ebef27619fae9e943bf51b5
Luis <lfegea@aol.com>
2020-06-16 13:42:59 -0700

    Updates

---
+++ precache-manifest.104821a554ddd938dfda74e1b6243867.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f67c8e8935d60c33eb01",
+    "url": "/Color-Tokens/static/css/main.1bd1f612.chunk.css"
+  },
+  {
+    "revision": "f67c8e8935d60c33eb01",
+    "url": "/Color-Tokens/static/js/main.f67c8e89.chunk.js"
+  },
+  {
+    "revision": "1a408848b34df688243f",
+    "url": "/Color-Tokens/static/css/1.ccbd3be7.chunk.css"
+  },
+  {
+    "revision": "1a408848b34df688243f",
+    "url": "/Color-Tokens/static/js/1.1a408848.chunk.js"
+  },
+  {
+    "revision": "24b12a8e70eac55721a1",
+    "url": "/Color-Tokens/static/js/runtime~main.24b12a8e.js"
+  },
+  {
+    "revision": "d1ee5080e4f28cb63bfa83aa55e52eca",
+    "url": "/Color-Tokens/index.html"
+  }
+];
